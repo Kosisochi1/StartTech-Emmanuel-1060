87b9edb0a0bc12e1dff4657e86b5da79ab935bbf
Kosisochi1 <emmanuelezeoyirikosi@yahoo.com>
2026-01-28 02:55:19 +0100

    chores/27th-night

--- starttech-application/Client/src/lib/apiClient.ts
+++ starttech-application/Client/src/lib/apiClient.ts
@@ -4,6 +4,6 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true,
+  withCredentials: false,
   // Crucial for httpOnly cookies
 });
